9a1b01ddfbdc0b6e34d706e45a405013b7f52b63
sbksocialclub-cloud <sbksocialclub@gmail.com>
2026-08-15 22:48:22 +0200

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'chattaiko-v2';
+const urlsToCache = ['/','/index.html','/admin.html','/logo.svg','/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
