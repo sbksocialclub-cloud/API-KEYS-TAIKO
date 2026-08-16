5af062cbbee403c9105c6b65c8125f9349be89ca
sbksocialclub-cloud <sbksocialclub@gmail.com>
2026-08-16 06:59:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: '1mb' }));
-app.use(express.static('.'));
+app.use(express.static('public')); // ✅ busca en public/
+
 
 const PORT = process.env.PORT || 8080;
 
